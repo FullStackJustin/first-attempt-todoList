9d1e20b044dc141274a748f7fbba8bd7a90966e2
FullStackJustin <ju.tay.lor2019@gmail.com>
2022-04-16 02:10:01 +0000

    first attempt

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,98 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState, useEffect, useRef } from "react";
 
 //create your first component
-const Home = () => {
-	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+ const Home = () => {
+	const Home = () => {
+		const [todos, setTodos] = useState([
+			{ text: "Do homework.", isEdit: false },
+		]);
+		const inputRefs = useRef(null);
+		const [textValue, setTextValue] = useState("");
+		const add = () => {
+			console.log("click");
+			let newArray = [...todos];
+			newArray.push({ text: inputRefs.current.value, isEdit: false });
+			console.log(inputRefs.current.value);
+			console.log(newArray);
+			setTodos(newArray);
+		};
+		const handleInput = (event) => {
+			setTextValue(event.target.value);
+			console.log(event.target.value);
+		};
+		useEffect(() => {}, [inputRefs]);
+		return (
+			<div>
+				<div>
+					<input
+						ref={inputRefs}
+						// onChange={(event) => {
+						//  setTextValue(event.target.value);
+						// }}
+					/>
+					<button type="button" onClick={add}>
+						Add
+					</button>
+				</div>
+				{todos.map((todo, index) => {
+					return (
+						<div key={index}>
+							{todo.text}
+							<div>
+								<button type="button" onClick={""}>
+									Edit
+								</button>
+								<button type="button" onClick={""}>
+									Delete
+								</button>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		);
+	};
+// 	const [toDos, setToDos] = useState([
+// 		{ text: "Things to do", editted: false },
+// 	]);
+	
+// 	const inputRef = useRef(null);
+// 	const [textValue, setTextValue] = useState("");
+// 	const add = () => {
+// 		let newList = [...toDos];
+// 		newList.push({ text: inputRef.current.value, eddited: false });
+// 		setToDos(newList);
+// 	};
+// 	const handleInput = (event) => {
+// 		setTextValue(event.target.value);
+// 		console.log(event.target.value);
+// 	};
+// 	useEffect(() => {}, [inputRef]);
+// 	return (
+// 		<div>
+// 			<div>
+// 				<input ref={inputRef}/>
+// 				<button type="button" onClick={add}>
+// 					Add to list
+// 				</button>
+// 			</div>
+// 			{toDos.map((toDos, index) => {
+// 				return (
+// 					<div key={index}>
+// 						{toDos.text}
+// 						<div>
+// 							<button type="button" onClick={""}>
+// 								Edit
+// 							</button>
+// 							<button type="button" onClick={""}>
+// 								Delete
+// 							</button>
+// 						</div>
+// 					</div>
+// 				);
+// 			})}
+// 		</div>
+// 	);
+// };
 
 export default Home;
